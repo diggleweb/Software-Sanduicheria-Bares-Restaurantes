b73605a557e5a25ba51e5fddf4bbd1a1525d8b42
GabrielDvT <gabriel.dvt@hotmail.com>
2017-01-23 17:21:25 -0200

    Criado cadastro de novos itens, filtrar, e alterado no banco

---
+++ laravel/resources/views/adicionar/item.blade.php
@@ -0,0 +1,77 @@
+@extends('comum')
+
+@section('titulo')
+{{'Novo Item'}}
+@endsection
+
+@section('corpo')
+
+<script>
+$(document).ready(
+	function() {
+		$('[name="nome"]').focus();
+		$('[name="precoCompra"]').maskMoney({prefix: "R$ "});
+		$('[name="precoVenda"]').maskMoney({prefix: "R$ "});
+	}
+);
+
+//assim que submeter o formulario, devemos retirar a mascara de dinheiro do campo salario (para evitar que seja enviado R$ 1023123.1231,00 por exemplo)
+$(function() {
+	$('#formulario').submit(
+		function() {
+			$('[name="precoCompra"]').val($('[name="precoCompra"]').maskMoney('unmasked')[0]);
+			$('[name="precoVenda"]').val($('[name="precoVenda"]').maskMoney('unmasked')[0]);
+		}
+	);
+})
+</script>
+
+<div class="page-header">
+	<h1>Bem-vindo, administrador!</h1>
+</div>
+
+{{-- BreadCrumb --}}
+<ol class="breadcrumb">
+ 	 <li><a href="/">Garçom</a></li>
+  	<li class="active"><a href="/administrador">Administrador</a></li>
+  	<li class="active"><a href = "/administrador/listarItens">Listar Itens</a></li>
+  	<li class="active">Novo Item</li>
+</ol>
+
+
+<div class = "container">
+<h1>Adicionar um novo Item</h1>
+
+<br>
+
+@if(Session::has('mensagem')) 
+	<div class = "alert alert-success" role = "alert"> {{Session::get('mensagem')}} </div>
+@endif
+
+@if($errors->any()) 
+	<div class = "alert alert-danger" role = "alert"> {{$errors->first()}} </div>
+@endif
+
+{{-- Formulário --}}
+{!! Form::open(array('id' => 'formulario', 'url' => '/administrador/salvarItem', 'method' => 'POST', 'onSubmit' => 'action(this)', 'files' => true)) !!}
+	{!! Form::label('Nome: ') !!}
+	{!! Form::text('nome', '', array('class' => 'form-control', 'style' => 'width: 500px', 'maxlength' => '200')) !!}
+	<br>
+	{!! Form::label('Preço de Compra: ') !!}
+	{!! Form::text('precoCompra', '', array('class' => 'form-control', 'style' => 'width: 150px', 'maxlength' => '15')) !!}
+	<br>
+	{!! Form::label('Preço de Venda: ') !!} <br>
+	{!! Form::text('precoVenda', '', array('class' => 'form-control', 'style' => 'width: 150px', 'maxlength' => '15')) !!}
+	<br>
+	<br>
+	{!! Form::label('Imagem: ') !!} <br>
+	<input type = "file" name = "urlImagem" accept="image/*"/>
+	<br><br>
+	<a href = "/administrador/listarItens" style = "margin-left: 300px; width: 100px" class = "btn btn-default">Cancelar</a>
+	{!! Form::submit('Salvar', array('class' => 'btn btn-primary', 'style' => 'margin-left: 30px; width: 100px')) !!}
+{!! Form::close()!!}
+
+</div>
+
+@endsection
+
